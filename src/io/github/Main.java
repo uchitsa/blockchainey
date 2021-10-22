6ac6d9b0ddaf446c15264053f8059ba91e1a3047
uchitsa <uchitsa@ya.ru>
2021-10-23 02:34:46 +0300

    add wallet examples

--- src/io/github/Main.java
+++ src/io/github/Main.java
@@ -5,19 +5,25 @@ import java.util.ArrayList;
 public class Main {
 
     public static ArrayList<Block> blockchain = new ArrayList<>();
+    public static ArrayList<Transaction> transactions = new ArrayList<>();
     public static int difficulty = 5;
 
     public static void main(String[] args) {
-        Block block = new Block(0, null, "First Block");
+        Block block = new Block(0, null, transactions);
         block.mineBlock(difficulty);
         blockchain.add(block);
-        System.out.println(block.toString());
+        System.out.println(block);
 
-        Block block2 = new Block(1, block.getCurrHash(), "Second Block");
+        Block block2 = new Block(1, block.getCurrHash(), transactions);
         block2.mineBlock(difficulty);
         blockchain.add(block2);
-        System.out.println(block2.toString());
+        System.out.println(block2);
         System.out.println("Current chain valid: " + validChain(blockchain));
+
+        Wallet walletA = new Wallet(blockchain);
+        Wallet walletB = new Wallet(blockchain);
+        System.out.println("Wallet A balance: " + walletA.getBalance());
+        System.out.println("Wallet B balance: " + walletB.getBalance());
     }
 
     public static boolean validChain(ArrayList<Block> blockchain) {
@@ -35,15 +41,13 @@ public class Main {
         if (oldBlock == null) {
             if (newBlock.getIndex() != 0) return false;
             if (newBlock.getPrevHash() != null) return false;
-            if (newBlock.getCurrHash() == null || !newBlock.calcHash().equals(newBlock.getCurrHash())) return false;
-            return true;
+            return newBlock.getCurrHash() != null && newBlock.calcHash().equals(newBlock.getCurrHash());
         } else {
             if (newBlock != null) {
                 if (oldBlock.getIndex() + 1 != newBlock.getIndex()) return false;
                 if (newBlock.getPrevHash() == null || !newBlock.getPrevHash().equals(oldBlock.getCurrHash()))
                     return false;
-                if (newBlock.getCurrHash() == null || !newBlock.calcHash().equals(newBlock.getCurrHash())) return false;
-                return true;
+                return newBlock.getCurrHash() != null && newBlock.calcHash().equals(newBlock.getCurrHash());
             }
             return false;
         }
